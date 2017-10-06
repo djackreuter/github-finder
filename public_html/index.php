2668555630b5fd3e592e3fdec5513088535038bc
Dylan Reuter <djreuter45@gmail.com>
2017-10-06 16:20:56 -0600

    updated html for app

--- public_html/index.php
+++ public_html/index.php
@@ -21,7 +21,7 @@
 		<title>Github Finder</title>
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse navbar-fixed-top">
+		<nav class="navbar navbar-inverse">
 			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -30,22 +30,22 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Project name</a>
+					<a class="navbar-brand" href="#">Github Finder</a>
 				</div>
 				<div id="navbar" class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#contact">Contact</a></li>
+
 					</ul>
 				</div><!--/.nav-collapse -->
 			</div>
 		</nav>
 		<div class="container">
-			<div class="starter-template">
-				<h1>Bootstrap starter template</h1>
-				<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-			</div>
+			<div class="search-container">
+				<h1>Search Github Users</h1>
+				<p class="lead">Enter a username to fetch a users profile info and repos</p>
+				<input type="text" id="search-user" class="form-control" placeholder="Github Username...">
+			</div><!-- /.search-container -->
+			<div id="profile"></div>
 		</div><!-- /.container -->
 	</body>
 </html>
