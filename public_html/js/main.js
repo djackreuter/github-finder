abaaba6f56d90515146127571fe06642526f74a2
Dylan Reuter <djreuter45@gmail.com>
2017-10-08 13:08:52 -0600

    adding profile info

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -12,22 +12,30 @@ $(document).ready(function() {
 			}
 		}).done(function(user) {
 			$('#profile').html(`
-							<div class="panel panel-default">
+				<div class="panel panel-default">
 				  <div class="panel-heading">
 					 <h3 class="panel-title">${user.name}</h3>
-				  </div>
+				  </div><!-- /.panel-heading -->
 				  <div class="panel-body">
-					 <div  class="row">
+					 <div class="row">
 					 	<div class="col-md-3">
 					 		<img class="thumbnail avatar" src="${user.avatar_url}" alt="Github user image">
-						</div>
+					 		<a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+						</div><!-- /.col-md-3 -->
 						<div class="col-md-9">
+						<span class="label label-default">Public Repos: ${user.public_repos}</span>
+						<span class="label label-primary">Public Gists: ${user.public_gists}</span>
+						<span class="label label-success">Followers: ${user.followers}</span>
+						<span class="label label-info">Following: ${user.following}</span>
+						<br><br>
+						<ul class="list-group">
 						
-						</div>
-					</div>
-				  </div>
-				</div>
-			`)
+						</ul>
+						</div><!-- /.col-md-9 -->
+					</div><!-- /.row -->
+				  </div><!-- /.panel-body -->
+				</div><!-- /.panel panel-default -->
+			`);
 		});
 	});
 });
